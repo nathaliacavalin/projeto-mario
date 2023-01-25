30ab7e21d47c291197d375b9d21812936a102b34
Nathália Cavalin <118205102+nathaliacavalin@users.noreply.github.com>
2023-01-25 18:27:21 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModa(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener ("click", () => {
+    alternarModa();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto");
+    video.setAttribute("src", "");
+});
